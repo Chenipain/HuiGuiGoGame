f2bb3c2837b134e2284f51b4aed5ee160c429563
mederic <mederic18@gmail.com>
2017-11-28 13:05:42 +0000

    Go Main

--- src/Go.java
+++ src/Go.java
@@ -1,7 +1,3 @@
-//an experiment to see how much JavaFX code is required
-//to build a game of reversi
-
-//imports
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
@@ -21,35 +17,35 @@ import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 
 //class defnition for reversi game
-public class Reversi extends Application {
-	// overridden init method
-	public void init() {
+public class Go extends Application {
+    // overridden init method
+    public void init() {
         sp_mainlayout = new StackPane();
         rc_reversi = new GoControl();
         sp_mainlayout.getChildren().add(rc_reversi);
-	}
-	
-	// overridden start method
-	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Reversi");
-		primaryStage.setScene(new Scene(sp_mainlayout, 800, 800 ));
-		primaryStage.show();
-	}
-	
-	// overridden stop method
-	public void stop() {
-		
-	}
-	
-	// entry point into our program for launching our javafx applicaton
-	public static void main(String[] args) {
-		launch(args);
-	}
-	
-	// private fields for a stack pane and a reversi control
-	private StackPane sp_mainlayout;
-	private GoControl rc_reversi;
-	
+    }
+
+    // overridden start method
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Go");
+        primaryStage.setScene(new Scene(sp_mainlayout, 800, 800 ));
+        primaryStage.show();
+    }
+
+    // overridden stop method
+    public void stop() {
+
+    }
+
+    // entry point into our program for launching our javafx applicaton
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    // private fields for a stack pane and a reversi control
+    private StackPane sp_mainlayout;
+    private GoControl rc_reversi;
+
 }
 
 //class definition for a custom reversi control
